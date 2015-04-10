effafc92c0e5f85e575953019f6109d3683e84a2
chetankabra <chetankabra8@gmail.com>
2015-04-10 16:15:09 -0600

    Merge remote-tracking branch 'remotes/Nispand1492/master'
    Conflicts:
    	app/src/main/res/layout/activity_calenderactivity.xml

--- app/src/main/java/com/example/nispand/smartzcompanion/CalEvent.java
+++ app/src/main/java/com/example/nispand/smartzcompanion/CalEvent.java
@@ -1,28 +1,63 @@
 package com.example.nispand.smartzcompanion;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Toast;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
-
+import java.lang.String;
 
 public class CalEvent extends ActionBarActivity {
     CalendarView calendar;
+    CalendarContract Calendars;
+    Button Add, Search ,Delete;
+    long cday;
+    long cmonth;
+    long cyear;
+    long xyz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cal_event);
-        initializeCalendar();
+        Add = (Button) findViewById(R.id.Add);
+        Search = (Button) findViewById(R.id.Search);
+        Delete = (Button)findViewById(R.id.Delete);
+        calendar = (CalendarView) findViewById(R.id.calendarView2);
+        Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar.setVisibility(View.VISIBLE);
+                initializeCalendar();
+                add_event();
+            }
+        });
+        Search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar.setVisibility(View.VISIBLE);
+                initializeCalendar();
+                search_event();
+            }
+        });
+        Delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar.setVisibility(View.VISIBLE);
+                initializeCalendar();
+            }
+        });
     }
     public void initializeCalendar() {
-        calendar = (CalendarView) findViewById(R.id.calendarView2);
 
         // sets whether to show the week number.
         calendar.setShowWeekNumber(false);
@@ -44,14 +79,13 @@ public class CalEvent extends ActionBarActivity {
         calendar.setSelectedDateVerticalBar(R.color.darkgreen);
 
 
-
-        //sets the listener to be notified upon selected date change.
+    }
+    public void add_event()
+    {
+        //sets the listener to be notified upon selected date change
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             //show the selected date as a toast
             public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
-                int cday = day;
-                int cmonth = month;
-                int cyear = year;
                 Toast.makeText(getApplicationContext(), day + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
                 Calendar cal = Calendar.getInstance();
                 Intent intent = new Intent(Intent.ACTION_INSERT);
@@ -67,28 +101,67 @@ public class CalEvent extends ActionBarActivity {
                 startActivity(intent);
             }
         });
-
     }
+    public void search_event(){
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            //show the selected date as a toast
+            public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
+                Toast.makeText(getApplicationContext(), day + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+                long date1 = calendar.getDate();
+                Date d2 = new Date(date1);
+                String cday = (String)android.text.format.DateFormat.format("date",d2);
+                String cmonth = (String) android.text.format.DateFormat.format("MMM", d2);
+                System.out.print(""+cday+""+cmonth);
+                String[] projection = { CalendarContract.Events._ID,
+                        CalendarContract.Events.TITLE,CalendarContract.Events.DTSTART };
+                // Get a Cursor over the Events Provider.
+                Cursor cursor = getContentResolver().query(
+                        CalendarContract.Events.CONTENT_URI, projection, null, null,
+                        null);
+                // Get the index of the columns.
+                int nameIdx = cursor
+                        .getColumnIndexOrThrow(CalendarContract.Events.TITLE);
+                int idIdx = cursor.getColumnIndexOrThrow(CalendarContract.Events._ID);
+                int D1 = cursor.getColumnIndexOrThrow(CalendarContract.Events.DTSTART);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_cal_event, menu);
-        return true;
+                // Initialize the result set.
+                String[] result = new String[cursor.getCount()];
+                // Iterate over the result Cursor.
+                while (cursor.moveToNext()) {
+                    Date d1 = new Date (cursor.getLong(D1));
+                    String ccday = (String)android.text.format.DateFormat.format("date",d1);
+                    String ccmonth = (String) android.text.format.DateFormat.format("MMM", d1);
+                    // Extract the name.
+                    String name = cursor.getString(nameIdx);
+                    // Extract the unique ID.
+                    String id = cursor.getString(idIdx);
+                    result[cursor.getPosition()] = name + "(" + id + ")";
+                    if (cday.compareTo(ccday)==0 & cmonth.compareTo(ccmonth)==0) {
+                        Toast.makeText(getApplicationContext(), name + "(" + id + ")" + "(" + d1 + ")", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
     }
+            public boolean onCreateOptionsMenu(Menu menu) {
+                // Inflate the menu; this adds items to the action bar if it is present.
+                getMenuInflater().inflate(R.menu.menu_cal_event, menu);
+                return true;
+            }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            @Override
+            public boolean onOptionsItemSelected(MenuItem item) {
+                // Handle action bar item clicks here. The action bar will
+                // automatically handle clicks on the Home/Up button, so long
+                // as you specify a parent activity in AndroidManifest.xml.
+                int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                //noinspection SimplifiableIfStatement
+                if (id == R.id.action_settings) {
+                    return true;
+                }
+
+                return super.onOptionsItemSelected(item);
+            }
         }
-
-        return super.onOptionsItemSelected(item);
-    }
-}
